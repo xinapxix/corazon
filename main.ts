fac5746dad527fd3572f72e0c78fd221af712ed4
xinapxix <xinapxix@gmail.com>
2021-05-05 19:48:15 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
